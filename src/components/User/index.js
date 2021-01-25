b9f48237af0dec232c28cf8515707f213eb64daf
Dmitriy <dm.min.zp@gmail.com>
2021-01-25 13:00:14 +0200

    fix bootstrap connect

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "bootstrap/dist/css/bootstrap.min.css";
 import './User.css';
 
 class User extends Component {
